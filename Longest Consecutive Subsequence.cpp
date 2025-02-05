45fe355414269d249e34c7b3fcd706eaff033f10
Muralidhar Maddali <143109142+muralidharmaddali@users.noreply.github.com>
2025-02-05 14:11:50 +0530

    Create Longest Consecutive Subsequence.cpp

---
+++ Longest Consecutive Subsequence.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Function to return length of longest subsequence of consecutive integers.
+    int longestConsecutive(vector<int>& arr) {
+        // Your code here
+        unordered_set<int> st(arr.begin(),arr.end());
+        int cnt = 0;
+        
+        for(int i:st)
+        {
+            if(!st.count(i - 1))
+            {
+                int p = i,inc = 1;
+                while(st.count(p+1)) p++, inc++;
+                cnt = max(cnt,inc++);
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.longestConsecutive(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
