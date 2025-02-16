cb3112a7513f2ad3ec9a37ec8ad68683be4ee6cb
Muralidhar Maddali <143109142+muralidharmaddali@users.noreply.github.com>
2025-02-16 09:45:59 +0530

    Create Count Pairs whose sum is less than target.cpp

---
+++ Count Pairs whose sum is less than target.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Your code here
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        int left=0,right=n-1,cnt=0;
+        while(left<right)
+        {
+            int sum = arr[left] + arr[right];
+            if(sum<target)
+            {
+                cnt+= (right-left);
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution ob;
+        int res = ob.countPairs(arr, target);
+        cout << res << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
