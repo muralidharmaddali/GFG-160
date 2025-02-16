e8b11604a5a560f644a441bebd22cb7f734e630c
Muralidhar Maddali <143109142+muralidharmaddali@users.noreply.github.com>
2025-02-16 10:52:38 +0530

    Create Subarray with given sum.cpp

---
+++ Subarray with given sum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+        // code here
+        int n=arr.size();
+        int sum=0,pre=0;
+        for(int i=0;i<n;i++)
+        {
+            pre+=arr[i];
+            while(pre>target && sum <=i) pre-=arr[sum++];
+            if(pre == target) return {sum+1,i+1};
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        int d;
+        string input;
+
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        cin >> d;
+        cin.ignore();
+
+        Solution ob;
+        vector<int> result = ob.subarraySum(arr, d);
+        for (int i : result) {
+            cout << i << " ";
+        }
+        cout << "\n~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
