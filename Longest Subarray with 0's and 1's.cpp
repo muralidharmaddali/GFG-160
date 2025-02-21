277bb6a42d91565643f83c133301b1b043ed3ea2
Muralidhar Maddali <143109142+muralidharmaddali@users.noreply.github.com>
2025-02-21 20:15:38 +0530

    Create Longest Subarray with 0's and 1's.cpp

---
+++ Longest Subarray with 0's and 1's.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxLen(vector<int> &arr) {
+        // Your code here
+        unordered_map<int,int> ump;
+        int sum=0,len=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            sum+=(arr[i]==0)?-1:1;
+            if(sum==0)len=i+1;
+            if(ump.count(sum))len=max(len,i-ump[sum]);
+            else ump[sum]=i;
+        }
+        return len;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore(); // To ignore the newline character after reading T
+
+    while (T--) {
+        string line;
+        getline(cin, line); // Read the whole line for the array
+
+        // Convert the line into an array of integers
+        stringstream ss(line);
+        vector<int> a;
+        int num;
+        while (ss >> num) {
+            a.push_back(num);
+        }
+
+        // Create the solution object
+        Solution obj;
+
+        // Call the maxLen function and print the result
+        cout << obj.maxLen(a) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
