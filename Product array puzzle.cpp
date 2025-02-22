4a10304ee19086d925e81414a50ff46e3c0b570d
Muralidhar Maddali <143109142+muralidharmaddali@users.noreply.github.com>
2025-02-22 20:45:19 +0530

    Create Product array puzzle.cpp

---
+++ Product array puzzle.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        // code here
+        int n=arr.size(),prod=1,cnt=count(arr.begin(),arr.end(),0);
+        if(cnt>1) return vector<int>(n,0);
+        for(int j:arr) if(j) prod*=j;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=cnt? (arr[i]?0:prod):prod/arr[i];
+        }
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        vector<int> res = obj.productExceptSelf(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
